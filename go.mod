1ceaf15679aeea03757357a9c2445b54f3e68065
AlexPan <alexpan@AlexPans-MacBook-Pro.local>
2019-10-30 20:46:58 +0800

    Initialize go mod

---
+++ go.mod
@@ -0,0 +1,10 @@
+module qlog
+
+go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.4.0
+	github.com/spf13/viper v1.4.0
+	go.uber.org/zap v1.12.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+)
--- logger.go
+++ logger.go
@@ -26,6 +26,27 @@ const (
 	defaultLogFormat     = "json"                // defaultLogFormat is the default format of log files.
 )
 
+// LogFileConfig serializes file log with config in yaml/json.
+type LogFileConfig struct {
+	Filename   string `mapstructure:"file_name"`
+	MaxSize    int    `mapstructure:"max_size"`
+	MaxDays    int    `mapstructure:"max_days"`
+	MaxBackups int    `mapstructure:"max_backups"`
+}
+
+// LogConfig serializes log with yaml/json.
+type LogConfig struct {
+	Level               string         `mapstructure:"level"`
+	Encoding            string         `mapstructure:"encoding"`
+	Format              string         `mapstructure:"format"`
+	DisableTimestamp    bool           `mapstructure:"disable_timestamp" json:"disable-timestamp"`
+	File                *LogFileConfig `mapstructure:"log_file"`
+	Development         bool           `mapstructure:"development"`
+	DisableCaller       bool           `mapstructure:"disable_caller"`
+	DisableStacktrace   bool           `mapstructure:"disable_stacktrace"`
+	DisableErrorVerbose bool           `mapstructure:"disable_error_verbose"`
+}
+
 //EchoTimeEncoder set time format
 func EchoTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
 	enc.AppendString(t.Format(defaultLogTimeFormat))
